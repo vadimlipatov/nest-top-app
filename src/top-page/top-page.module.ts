2a84749feaf25f4163f11061dadd895b57b234c0
Admin <admin@MacBook-Pro-Admin.local>
2023-10-17 19:46:29 +0600

    add top page service

--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { TopPageController } from './top-page.controller';
 import { TopPageModel } from './top-page.model';
+import { TopPageService } from './top-page.service';
 
 @Module({
   controllers: [TopPageController],
+  providers: [TopPageService],
   imports: [
     TypegooseModule.forFeature([
       {
